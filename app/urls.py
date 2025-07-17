ece2a77f572dfaf7ffa9c31072fff53294f0f1d5
Prajal Maharjan <prajalmhrzn@gmail.com>
2025-07-17 10:09:21 +0545

    Added pagination

--- app/urls.py
+++ app/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.http import HttpResponse
 
 
 schema_view = get_schema_view(
@@ -33,8 +34,11 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
+def homeview(request):
+    return HttpResponse("Server is working")
 
 urlpatterns = [
+    path("", homeview),
     path('admin/', admin.site.urls),
     path('user/',include("user.urls")),
     path('product/',include("product.urls")),
